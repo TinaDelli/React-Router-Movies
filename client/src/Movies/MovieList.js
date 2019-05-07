dd04777e3d2537944dc606e1e656177ca69da79b
TinaDelli <cecurrier92@gmail.com>
2019-05-07 18:28:14 -0400

    added in moviecard for my data

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import {Link,Route} from 'react-router-dom';
+import MovieCard from './MovieCard';
 import axios from 'axios';
 
 export default class MovieList extends Component {
@@ -33,25 +34,13 @@ export default class MovieList extends Component {
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
+  // const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card">
-    <Link to={`/movies/${movie.id}`}>
-      <h2>{title}</h2>
-      </Link>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
+    <Route 
+    exact
+    path="/"
+    render= {props => <MovieCard {...props} movie={movie} />}
+    
+    />
   );
 }
